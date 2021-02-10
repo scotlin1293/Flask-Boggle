a717e5b8d3dbb9a53596f5f7775e49ba0a500a65
Scotlin White <scotlin23@hotmail.com>
2021-02-10 09:31:35 -0800

    Flask-Boggle Exercise

---
+++ app.py
@@ -0,0 +1,45 @@
+from flask import Flask, request, render_template, jsonify, session
+from boggle import Boggle
+
+app = Flask(__name__)
+app.config["SECRET_KEY"] = "secret"
+boggle_game = Boggle()
+
+@app.route("/")
+def homepage():
+    #Add board to homepage; apply highest score and amount of plays.
+
+    board = boggle_game.make_board()
+    session['board'] = board
+    highscore = session.get("highscore", 0)
+    nplays = session.get("nplays", 0)
+
+    return render_template("index.html", board=board,
+                           highscore=highscore,
+                           nplays=nplays)
+
+
+@app.route("/check-word")
+def check_word():
+   #Check if the entered word is on the board(in dict) 
+
+    word = request.args["word"]
+    board = session["board"]
+    response = boggle_game.check_valid_word(board, word)
+
+    return jsonify({'result': response})
+
+
+@app.route("/post-score", methods=["POST"])
+def post_score():
+    #Receive new score, update amount of plays & highschore if higher than previous.
+
+    score = request.json["score"]
+    highscore = session.get("highscore", 0)
+    nplays = session.get("nplays", 0)
+
+    session['nplays'] = nplays + 1
+    session['highscore'] = max(score, highscore)
+
+    return jsonify(brokeRecord=score > highscore)
+
